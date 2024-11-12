934c93205443f67c3450d71a9cb2c5306aca7e54
AlbertoGandarillas <albertogandarillas@hotmail.com>
2024-11-12 10:21:49 -0500

    Vercel settings

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('CPL API')
-    .setDescription('API documentation for CPL College Landing pages and CPL Dashboard projects')
+    .setDescription(
+      'API documentation for CPL College Landing pages and CPL Dashboard projects',
+    )
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
