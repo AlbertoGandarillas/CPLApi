9ced8abbaf5ff8e7955cc0a284496e6153c793f8
AlbertoGandarillas <albertogandarillas@hotmail.com>
2024-11-12 10:08:54 -0500

    First commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('CPL API')
+    .setDescription('API documentation for CPL College Landing pages and CPL Dashboard projects')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3099);
+}
+bootstrap();
