7c6af25383b652e55ea68ef771e4b0c7983d9503
AlbertoGandarillas <albertogandarillas@hotmail.com>
2024-11-12 12:35:32 -0500

    Test without swagger

--- src/main.ts
+++ src/main.ts
@@ -19,15 +19,15 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
-  const config = new DocumentBuilder()
-    .setTitle('CPL API')
-    .setDescription(
-      'API documentation for CPL College Landing pages and CPL Dashboard projects',
-    )
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('CPL API')
+  //   .setDescription(
+  //     'API documentation for CPL College Landing pages and CPL Dashboard projects',
+  //   )
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3099);
 }
